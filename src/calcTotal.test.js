268b6651f3a7de16a52421590e05df633423c378
Saajan More <saajanmore1995@gmail.com>
2024-11-21 12:58:15 +0530

    Checking calcTotal

--- src/calcTotal.test.js
+++ src/calcTotal.test.js
@@ -1,10 +1,12 @@
+import React from "react";
+
 const { fireEvent, screen, render } = require("@testing-library/react");
 import AddNumbers from "./AddNumbers";
 
 test.only("Add the Number String", () => {
   render(<AddNumbers />);
-  const calcStringTotal = screen.getByTestId("calcTotalbtn");
-  const totalValue = screen.getByTestId("totalValue");
+  const calcStringTotal = screen.getByTestId("calcTotalBtn");
+  expect(totalValue).toHaveTextContent("Total = 6");
 
   fireEvent.click(calcStringTotal);
 
